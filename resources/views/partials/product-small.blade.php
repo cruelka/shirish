4e3b9589c5e4ec6f20d0a63c9e5677dc970b7e35
Egor Karpachev <karpachev@gmail.com>
2019-11-06 13:48:35 +0600

    layout changes

---
+++ resources/views/partials/product-small.blade.php
@@ -0,0 +1,14 @@
+<div class="col s6 m2">
+    <div class=" card hoverable">
+        <div class="card-image">
+            <img class="responsive-img" src="/img/ab-bg.jpg">
+            <span class="card-title"> Title</span>
+        </div>
+        <div class="card-content">
+            <p>Product.</p>
+        </div>
+        <div class="card-action">
+            <a href="#">Open</a>
+        </div>
+    </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,44 +13,199 @@
                 </div>
             </div>
         </div>
-        <div class="section">
+        <div class="section container">
             <h5 class="center-align blue-grey-text darken-2 "> Header 1</h5>
 
 
-                <div class="blue darken-1 z-depth-1">
-                    <div id="deals-slider" class="row">
+            <div class="blue darken-1 z-depth-1">
+                <div id="deals-slider" class="row">
 
-                        @for($i = 0; $i < 5; $i ++)
-                            @include('partials.product')
-                        @endfor
-                    </div>
+                    @for($i = 0; $i < 5; $i ++)
+                        @include('partials.product')
+                    @endfor
                 </div>
+            </div>
+
+        </div>
+        <div class="section container">
+            <h5 class="center-align blue-grey-text darken-2 "> Header 2</h5>
+
+            <div class="blue darken-1 ">
+                <div id="promo-slider" class="row">
+                    @for($i = 0; $i < 5; $i ++)
+                        @include('partials.product')
+                    @endfor
+                </div>
+            </div>
+            <div class="divider"></div>
 
         </div>
         <div class="section">
-            <h5 class="center-align blue-grey-text darken-2 "> Header 2</h5>
 
-            <div class="row">
-                @for($i = 0; $i < 3; $i ++)
-                    @include('partials.product')
-                @endfor
+            <h5 class="left-align"> Header 2</h5>
+
+            <div>
+                <div id="small-slider" class="row">
+                    @for($i = 0; $i < 7; $i ++)
+                        @include('partials.product-small')
+                    @endfor
+                </div>
             </div>
+            <div class="divider"></div>
+        </div>
+    <div class="section">
+        <div class="row">
+            <div class="col s12 m4">
+                <div class="card blue-grey darken-1">
+                    <div class="card-content white-text">
+                        <span class="card-title">Banner </span>
+                        <p>Description.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m4">
+                <div class="card blue-grey darken-1">
+                    <div class="card-content white-text">
+                        <span class="card-title">Banner </span>
+                        <p>Description.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m4">
+                <div class="card blue-grey darken-1">
+                    <div class="card-content white-text">
+                        <span class="card-title">Banner </span>
+                        <p>Description.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="divider"></div>
+    </div>
+    <div class="section">
+        <div class="row">
+            <div class="section-header">
+                <div class="row">
+                    <h5 class="left-align col s12 m6">Cookware</h5>
+                    <div class="right-align col s12 m6">
+                        <a class="waves-effect waves-light btn"><i class="material-icons left">cloud</i>button</a>
+                        <a class="waves-effect waves-light btn"><i class="material-icons left">cloud</i>button</a>
+                        <a class="waves-effect waves-light btn"><i class="material-icons left">cloud</i>button</a>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div id="big-slider" class="row">
+                    @for($i = 0; $i < 5; $i ++)
+                        @include('partials.product')
+                    @endfor
+                </div>
+            </div>
+            <div class="divider"></div>
+        </div>
+    </div>
+    <div class="section">
+        <div class="row">
+            <div class="section-header">
+                <div class="row">
+                    <h5 class="left-align col s12 m6">One more</h5>
+                    <div class="right-align col s12 m6">
+                        <a class="waves-effect waves-light btn"><i class="material-icons left">cloud</i>button</a>
+                        <a class="waves-effect waves-light btn"><i class="material-icons left">cloud</i>button</a>
+                        <a class="waves-effect waves-light btn"><i class="material-icons left">cloud</i>button</a>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div id="big-slider-2" class="row">
+                    @for($i = 0; $i < 5; $i ++)
+                        @include('partials.product')
+                    @endfor
+                </div>
+            </div>
+            <div class="divider"></div>
+        </div>
+    </div>
+    <div class="section">
+        <div class="row">
+            <div class="col s6 m2">
+                <div class="card blue-grey darken-1">
+                    <div class="card-content white-text">
+                        <span class="card-title">brand </span>
+                        <p>Description.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col s6 m2">
+                <div class="card blue-grey darken-1">
+                    <div class="card-content white-text">
+                        <span class="card-title">brand </span>
+                        <p>Description.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col s6 m2">
+                <div class="card blue-grey darken-1">
+                    <div class="card-content white-text">
+                        <span class="card-title">brand </span>
+                        <p>Description.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col s6 m2">
+                <div class="card blue-grey darken-1">
+                    <div class="card-content white-text">
+                        <span class="card-title">brand </span>
+                        <p>Description.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col s6 m2">
+                <div class="card blue-grey darken-1">
+                    <div class="card-content white-text">
+                        <span class="card-title">brand </span>
+                        <p>Description.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col s6 m2">
+                <div class="card blue-grey darken-1">
+                    <div class="card-content white-text">
+                        <span class="card-title">brand </span>
+                        <p>Description.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="divider"></div>
+    </div>
+        <div class="section">
+
+            <h5 class="left-align">Most Viewed</h5>
+
+            <div>
+                <div id="most-viewed-slider" class="row">
+                    @for($i = 0; $i < 7; $i ++)
+                        @include('partials.product-small-horizontal')
+                    @endfor
+                </div>
+            </div>
+            <div class="divider"></div>
         </div>
     </div>
 @endsection
 @section('scripts')
     <script>
-        $("#main-slider").slick({
+        $("#most-viewed-slider").slick({
             infinite: false,
             dots: true,
-            adaptiveHeight: true,
-            arrows:true,
+            slidesToShow: 4,
+            arrows: true,
             // the magic
             responsive: [{
 
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 3,
                     infinite: true
                 }
 
@@ -58,7 +213,34 @@
 
                 breakpoint: 600,
                 settings: {
-                    slidesToShow: 2,
+                    dots: true
+                }
+
+            }, {
+
+                breakpoint: 300,
+                settings: "unslick" // destroys slick
+
+            }]
+        });
+    </script>
+    <script>
+        $("#main-slider").slick({
+            infinite: false,
+            dots: true,
+            arrows: true,
+            // the magic
+            responsive: [{
+
+                breakpoint: 1024,
+                settings: {
+                    infinite: true
+                }
+
+            }, {
+
+                breakpoint: 600,
+                settings: {
                     dots: true
                 }
 
@@ -74,15 +256,142 @@
         $("#deals-slider").slick({
             dots: true,
             adaptiveHeight: true,
-            arrows:true,
-            slidesPerRow: 3,
+            arrows: true,
+            slidesToShow: 3,
 
-            // the magic
             responsive: [{
 
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 3,
+                    infinite: true
+                }
+
+            }, {
+
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    dots: true
+                }
+
+            }, {
+
+                breakpoint: 300,
+
+                settings: "unslick" // destroys slick
+
+            }]
+        });
+    </script>
+    <script>
+        $("#promo-slider").slick({
+            dots: true,
+            adaptiveHeight: true,
+            arrows: true,
+            slidesToShow: 3,
+
+            responsive: [{
+
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    infinite: true
+                }
+
+            }, {
+
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    dots: true
+                }
+
+            }, {
+
+                breakpoint: 300,
+
+                settings: "unslick" // destroys slick
+
+            }]
+        });
+    </script>
+    <script>
+        $("#big-slider").slick({
+            dots: true,
+            adaptiveHeight: true,
+            arrows: true,
+            slidesToShow: 3,
+
+            responsive: [{
+
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    infinite: true
+                }
+
+            }, {
+
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    dots: true
+                }
+
+            }, {
+
+                breakpoint: 300,
+
+                settings: "unslick" // destroys slick
+
+            }]
+        });
+    </script>
+    <script>
+        $("#big-slider-2").slick({
+            dots: true,
+            adaptiveHeight: true,
+            arrows: true,
+            slidesToShow: 3,
+
+            responsive: [{
+
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    infinite: true
+                }
+
+            }, {
+
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    dots: true
+                }
+
+            }, {
+
+                breakpoint: 300,
+
+                settings: "unslick" // destroys slick
+
+            }]
+        });
+    </script>
+    <script>
+        $("#small-slider").slick({
+            dots: true,
+            adaptiveHeight: true,
+            arrows: true,
+            slidesToShow: 6,
+
+            responsive: [{
+
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 6,
                     infinite: true
                 }
 
@@ -97,6 +406,7 @@
             }, {
 
                 breakpoint: 300,
+
                 settings: "unslick" // destroys slick
 
             }]
