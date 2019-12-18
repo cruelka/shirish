ad42737c16ad45c96c668b679dbe185133d52655
Egor Karpachev <kes1993@ya.ru>
2019-12-18 22:59:12 +0600

    auth scaffoldeing added

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/','Controller@index');
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
